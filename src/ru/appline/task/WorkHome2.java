f9628507fe00200a3d807f55cdb9e49565148003
DevOpsAT_Admin <F5-g6-h7>
2020-12-01 20:51:29 +0300

    init commit

---
+++ src/ru/appline/task/WorkHome2.java
@@ -0,0 +1,7 @@
+package ru.appline.task;
+
+public class WorkHome2 {
+    public static void main(String[] args) {
+        System.out.println("А это задание 2");
+    }
+}
---
+++ src/ru/appline/task/WorkHome3.java
@@ -0,0 +1,7 @@
+package ru.appline.task;
+
+public class WorkHome3 {
+    public static void main(String[] args) {
+        System.out.println("Несколько классов со статичным методом main не вызовут ошибку");
+    }
+}
